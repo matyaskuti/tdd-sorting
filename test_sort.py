bb63abb70859974c6400616badbdfab968e88e9b
Matyas Bela Kuti <matyas@bynder.com>
2018-05-29 15:36:35 +0200

    [RED] Test 2 element list, out of order (split flow+assignment)
    Of course the test fails because we return the original list, without
    actually sorting.

--- test_sort.py
+++ test_sort.py
@@ -13,6 +13,9 @@ class TestSort(unittest.TestCase):
     def test_sort_list_with_two_elements_in_order(self):
         self._assert_list_sorted([23, 42], [23, 42])
 
+    def test_sort_list_with_two_elements_out_of_order(self):
+        self._assert_list_sorted([42, 23], [23, 42])
+
     def _assert_list_sorted(self, list1, list2):
         self.assertListEqual(sort(list1), list2)
 
