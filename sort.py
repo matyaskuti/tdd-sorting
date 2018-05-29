f6f0e76a89d114bd54566762a2f23412e1ceaf56
Matyas Bela Kuti <matyas@bynder.com>
2018-05-29 16:16:51 +0200

    [REFACTOR] Extract conditional (if to while)

--- sort.py
+++ sort.py
@@ -1,10 +1,14 @@
 def sort(list_):
     index = 0
     while len(list_) > index+1:
-        if list_[index+1] < list_[index]:
+        if _is_out_of_order(list_, index):
             _swap_with_next(list_, index)
         index += 1
     return list_
+
+
+def _is_out_of_order(list_, index):
+    return list_[index+1] < list_[index]
 
 
 def _swap_with_next(list_, index):
