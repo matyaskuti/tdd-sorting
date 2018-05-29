fc145ccec30b61619350a5cf4f90e997a049bafa
Matyas Bela Kuti <matyas@bynder.com>
2018-05-29 15:50:34 +0200

    [GREEN] Add already passing 3 long list tests ()
    The next step is to work with lists having 3 elements. Of all the
    possible permutations the list that is already in order and the list
    that only has the first 2 elements swapped already pass with the current
    algorithm.

--- test_sort.py
+++ test_sort.py
@@ -16,6 +16,12 @@ class TestSort(unittest.TestCase):
     def test_sort_list_with_two_elements_out_of_order(self):
         self._assert_list_sorted([42, 23], [23, 42])
 
+    def test_sort_three_elements_list_in_order(self):
+        self._assert_list_sorted([1, 23, 42], [1, 23, 42])
+
+    def test_sort_three_elements_list_first_two_out_of_order(self):
+        self._assert_list_sorted([23, 1, 42], [1, 23, 42])
+
     def _assert_list_sorted(self, list1, list2):
         self.assertListEqual(sort(list1), list2)
 
