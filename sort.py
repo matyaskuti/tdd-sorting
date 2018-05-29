216c0026fe8a6204539a91a3d651745fc7362f35
Matyas Bela Kuti <matyas@bynder.com>
2018-05-29 16:15:33 +0200

    [REFACTOR] Extract swapping (if to while)

--- sort.py
+++ sort.py
@@ -2,6 +2,10 @@ def sort(list_):
     index = 0
     while len(list_) > index+1:
         if list_[index+1] < list_[index]:
-            list_[index], list_[index+1] = list_[index+1], list_[index]
+            _swap_with_next(list_, index)
         index += 1
     return list_
+
+
+def _swap_with_next(list_, index):
+    list_[index], list_[index+1] = list_[index+1], list_[index]
