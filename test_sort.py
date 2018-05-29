33a54c363588c49fe3a4c3aeb616eb9ff2258dcd
Matyas Bela Kuti <matyas@bynder.com>
2018-05-29 14:39:00 +0200

    [RED] Force the sort function to take a parameter (null to constant)
    We want to go towards sorting and returning an empty list. The first
    step is to make sure the sort function takes a parameter, while still
    returning None.

--- test_sort.py
+++ test_sort.py
@@ -4,8 +4,8 @@ from sort import sort
 
 
 class TestSort(unittest.TestCase):
-    def test_sort_does_nothing(self):
-        self.assertIsNone(sort())
+    def test_sort_empty_list(self):
+        self.assertIsNone(sort([]))
 
 
 if __name__ == '__main__':
