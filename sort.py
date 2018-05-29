1f476d521fcddf7b57dc022cf61c5f4d8412a742
Matyas Bela Kuti <matyas@bynder.com>
2018-05-29 16:01:30 +0200

    [GREEN] Change if to while (if to while)
    With the index variable extracted it is a simple changing of the word if
    to while and incrementing the index variable in each iteration.

--- sort.py
+++ sort.py
@@ -1,6 +1,7 @@
 def sort(list_):
     index = 0
-    if len(list_) > index+1:
+    while len(list_) > index+1:
         if list_[index+1] < list_[index]:
             list_[index], list_[index+1] = list_[index+1], list_[index]
+        index += 1
     return list_
