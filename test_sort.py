1c5db998b3cfe5d0f790888b80fb91f8a066a754
Matyas Bela Kuti <matyas@bynder.com>
2018-05-29 14:46:49 +0200

    [RED] Implement test for one-element-list (constant to variable)
    The test of course fails because the sort function always returns an
    empty list.

--- test_sort.py
+++ test_sort.py
@@ -7,6 +7,9 @@ class TestSort(unittest.TestCase):
     def test_sort_empty_list(self):
         self.assertListEqual(sort([]), [])
 
+    def test_sort_list_with_one_element(self):
+        self.assertListEqual(sort([42]), [42])
+
 
 if __name__ == '__main__':
     unittest.main()
