59577cbd90730986d315b83c8d8c3a8fe9e82f2f
Matyas Bela Kuti <matyas@bynder.com>
2018-05-29 14:54:46 +0200

    [GREEN] Test two elements in a list in order ()
    This testcase passes right away withouth having to write any more
    production code.

--- test_sort.py
+++ test_sort.py
@@ -10,6 +10,9 @@ class TestSort(unittest.TestCase):
     def test_sort_list_with_one_element(self):
         self._assert_list_sorted([42], [42])
 
+    def test_sort_list_with_two_elements_in_order(self):
+        self._assert_list_sorted([23, 42], [23, 42])
+
     def _assert_list_sorted(self, list1, list2):
         self.assertListEqual(sort(list1), list2)
 
